57b11d0d081cfd05daaea94e559a751f49c05ca1
rishabhsingh971 <rishabh.singh@smartprix.com>
2019-09-15 02:31:14 +0530

    update version to 0.1.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="persession",
-    version="0.1.0",
+    version="0.1.1",
     description="A wrapper on requests session with persistance and login functionalities",
     long_description=README,
     long_description_content_type="text/markdown",
