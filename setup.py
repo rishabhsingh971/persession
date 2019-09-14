f792307c28975d3f59e98ac4f97018703629e1af
rishabhsingh971 <rishabh.singh@smartprix.com>
2019-09-14 17:44:08 +0530

    add setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+"""setup.py"""
+import pathlib
+from setuptools import setup, find_packages
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="persession",
+    version="0.0.1",
+    description="A wrapper on requests session with persistance and login functionalities",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rishabhsingh971/persession",
+    author="Rishabh Singh",
+    author_email="rishabhsingh971@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=find_packages(),
+)
